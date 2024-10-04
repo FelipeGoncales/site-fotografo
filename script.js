2c3a4dedb2ac7dbcd0ab9978d8c20206271ba6a6
felipeggoncales <felipeggoncales22@gmail.com>
2024-10-04 15:00:39 -0300

    comentando codigo

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
+// Ao carregar o site, adicionar todas as fotos do portfólio
 document.addEventListener('DOMContentLoaded', function() {
-
     const secGaleria = document.getElementsByClassName('sec-galeria')[0];
 
     for (const dado of dados) {
@@ -31,61 +31,64 @@ document.addEventListener('DOMContentLoaded', function() {
 
         secGaleria.appendChild(div);
     }
-
-    const links = document.querySelectorAll('#nav-menu a');
-
-    links.forEach(link => {
-        link.addEventListener('click', function(event) {
-            event.preventDefault(); // Evita o comportamento padrão do link
-
-            const targetId = this.getAttribute('href');
-            const targetElement = document.querySelector(targetId);
-            const elementPosition = (targetElement.getBoundingClientRect().top + window.scrollY) - 45;
-            
-            if (link === links[1]) {
-                if (targetElement) {
-                    window.scrollTo({
-                        top: elementPosition - 38
-                    });
-                }
-            } else {
-                if (targetElement) {
-                    const offset = window.innerHeight / 2 - targetElement.offsetHeight / 2;
-    
-                    window.scrollTo({
-                        top: elementPosition - offset
-                    });
-                }
-            }
-        });
-    });
-
-
-    const submitButton = document.getElementById('button-submit'); 
-    const form = document.querySelector('form');
-    const text = document.getElementById('text-formulario-enviado');
-    const inputNome = document.getElementById('input-nome');
-    const inputEmail = document.getElementById('input-email');
-    const inputText = document.getElementById('input-textarea');
-
-    submitButton.addEventListener('click', function(event) {
-        if (inputNome.checkValidity() && inputEmail.checkValidity() && inputText.checkValidity()) {
-            text.textContent = 'Formulário enviado com sucesso!';
-
-            inputNome.value = '';
-            inputEmail.value = '';
-            inputText.value = '';
-
-            event.preventDefault();
-        } else {
-            text.textContent = 'Informações inválidas.';
-        }
-    })  
 })
 
+// Alterando a rolagem do scroll
+const links = document.querySelectorAll('#nav-menu a');
+
+links.forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault(); // Evita o comportamento padrão do link
+
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        const elementPosition = (targetElement.getBoundingClientRect().top + window.scrollY) - 45;
+        
+        if (link === links[1]) {
+            if (targetElement) {
+                window.scrollTo({
+                    top: elementPosition - 38
+                });
+            }
+        } else {
+            if (targetElement) {
+                const offset = window.innerHeight / 2 - targetElement.offsetHeight / 2;
+
+                window.scrollTo({
+                    top: elementPosition - offset
+                });
+            }
+        }
+    });
+});
+
+// Enviar formulário
+const submitButton = document.getElementById('button-submit'); 
+const form = document.querySelector('form');
+const text = document.getElementById('text-formulario-enviado');
+const inputNome = document.getElementById('input-nome');
+const inputEmail = document.getElementById('input-email');
+const inputText = document.getElementById('input-textarea');
+
+submitButton.addEventListener('click', function(event) {
+    if (inputNome.checkValidity() && inputEmail.checkValidity() && inputText.checkValidity()) {
+        text.textContent = 'Formulário enviado com sucesso!';
+
+        inputNome.value = '';
+        inputEmail.value = '';
+        inputText.value = '';
+
+        event.preventDefault();
+    } else {
+        text.textContent = 'Informações inválidas.';
+    }
+})  
+
+// Abrir imagem em tela cheia
 const divOverlay = document.getElementById('div-overlay');
 const img = divOverlay.querySelector('img');
 
+// Adicionando todos os endereços de imagem de uma lista
 const imagens = [];
 for (dado of dados) {
     imagens.push(dado.img)
@@ -100,6 +103,7 @@ function abrirImagem() {
     }
 }
 
+// Passar para o lado direito
 function imagemDireita() {
     let imgSrc;
     if (window.location.href === 'http://127.0.0.1:5500/index.html') {
@@ -118,6 +122,7 @@ function imagemDireita() {
     }
 }
 
+// Passar para o lado direito
 function imagemEsquerda() {
     let imgSrc;
     if (window.location.href === 'http://127.0.0.1:5500/index.html') {
@@ -136,21 +141,16 @@ function imagemEsquerda() {
     }
 }
 
+// Se clicar fora da imagem fecha
+window.addEventListener('click', function(e) {
+    if (e.target === divOverlay) {
+        divOverlay.style.display = 'none'
+    } 
+})
+
+// Fechar a imagem quando clicar no ícone de "x"
 function fecharImagem() {
     const divOverlay = document.getElementById('div-overlay');
 
     divOverlay.style.display = 'none';
-}
-
-function abrirFecharImagem() {
-    const divOverlay = document.getElementById('div-overlay');
-    const img = divOverlay.querySelector('img'); // Corrigido: acessa a imagem corretamente
-
-    if (divOverlay.style.display === 'none' || divOverlay.style.display === '') {
-        divOverlay.style.display = 'flex';
-        img.src = this.src; // 'this' deve se referir à imagem que foi clicada
-    } else {
-        divOverlay.style.display = 'none';
-        img.src = ''; // Limpa o src da imagem ao fechar
-    }
 }
